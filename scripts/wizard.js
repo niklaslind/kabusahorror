2e87b2b84870b21bad13a2149282e370bc7d7e3a
Niklas Lind <niklas.johan.lind@gmail.com>
2017-04-20 17:00:13 +0200

    refactor vocabulary to file

--- scripts/wizard.js
+++ scripts/wizard.js
@@ -1,62 +1,13 @@
 module.exports = function(robot) {
-
-
+  
   var _ = require('lodash');
   var handlebars = require('handlebars');
 
-
+  var vocabulary = require('./vocabulary');
+  
   function unknownCommand() {
     return "Sorry, I didn't understand that?";
   }
-
-  var vocabulary = {
-
-    look: function(player, world, params) {
-      var location = player.currentLocation;
-      return location.description(player) +
-        '\nI can see '+_.map(player.currentLocation.items, 'name')  +
-        '\nI can go ' + _.keys(player.currentLocation.exits);
-    },
-
-    go: function(player, world, params) {
-      if (_.isEmpty(params)) return "Go where?"
-      var newLocation = _.get( player.currentLocation.exits, params[0]);
-      if (!newLocation) return "I can't go "+params[0]
-      player.currentLocation = newLocation;
-      return newLocation.description(player);
-    },
-
-    pick: function (player, world, params) {
-      if (_.isEmpty(params)) return "Pick what?";
-      var item = _.find(player.currentLocation.items, {name: params.join(' ')});
-      if (!item) return "I can't pick "+params[0];
-      player.inventory.push(item);
-      _.pull(player.currentLocation.items, item);
-      return "Ok!";
-    },
-
-    drop: function (player, world, params) {
-      if (_.isEmpty(params)) return "Drop what?";
-      var item = _.find(player.inventory, {name: params.join(' ')});
-      if (!item) return "I'm not holding "+params.join(' ');
-      player.currentLocation.items.push(item);
-      _.pull(player.inventory, item);
-      return "Ok!";
-    },
-
-    inventory: function (player, world, params) {
-      return player.name+" is carrying "+_.map(player.inventory, 'name');
-    },
-
-    use: function (player, world, params) {
-      if (_.isEmpty(params)) return "Use what?";
-      var item = _.find(player.inventory, {name: params.join(' ')});
-      if (!item) return "I'm not holding "+params.join(' ');
-      return item.use(player, world, params);
-    }
-
-  };
-
 
 
   var items = {
