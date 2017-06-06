1c1613016f6ff1cf94433b9c2fb89046a5d22a7d
Niklas Lind <niklas.johan.lind@gmail.com>
2017-06-06 11:26:53 +0200

    mocha and chai for test

---
+++ test/hildaTest.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const describe = require('mocha').describe;
+const it = require('mocha').it;
+
+
+describe('Hildas place', function () {
+  it('When taking an ice creaem Hilda should say no', function(){
+    chai.expect("hej").to.equal("hopp");
+  });
+});
