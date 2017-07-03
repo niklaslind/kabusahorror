61b9cc4d2990a3cb43f5145b5f58ac7f2dd411ee
Niklas Lind <niklas.johan.lind@gmail.com>
2017-07-04 00:13:14 +0200

    refactor cleanup worldLoader

--- scripts/worldLoader.js
+++ scripts/worldLoader.js
@@ -87,41 +87,6 @@
     addGameLogic(world, defaultWorld.gameLogic);
     return world;
   }
-            
-  
-  function getRemoteWorld() {
-    // Get global world from api, then create world object.
-    //var globalWorldUrl = 'http://localhost:8050/getDataAws';
-    var globalWorldUrl = 'http://worldmaker.herokuapp.com/getDataAws';
-    
-    request(globalWorldUrl, function (error, response, body) {
-      if (error){
-        //console.log("Error getting world data, using backup world for now: ", error);
-        rooms = roomsBackup;
-      } else {
-        console.log("External update succeded, Using global world");
-        
-        //rooms = JSON.parse(JSON.parse(response.body).data);
-        var rooms = JSON.parse(JSON.parse(response.body).data);
-
-        // Items needs to be parsed separately, ToDo: Fix this.
-        for (var key in rooms){                
-          if (rooms[key].items[0].length > 0) {
-            var curObj = rooms[key].items[0].split(".")[1];          
-            rooms[key].items = [items[curObj]];        
-          }
-
-        }
-      }
-      
-      world =  {
-        map: prepareRooms(rooms),
-        items: items,
-        players: {}
-      };
-      
-    });
-  }
 
 
   module.exports = {
